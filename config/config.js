e5ab74239f61e4554115e11f5500ba0d90f28357
espazo <espazo@outlook.com>
2024-06-24 19:46:40 +0800

    feature: add puzzlecards page

--- config/config.js
+++ config/config.js
@@ -5,13 +5,30 @@ export default {
         component: '../layout/',
         routes: [
             {
+                path: 'puzzlecards',
+                component: './puzzlecards',
+            },
+            {
                 path: '/helloworld',
                 component: './HelloWorld',
-            }
+            },
+            {
+                path: '/',
+                component: './HelloWorld',
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    {path: 'analysis', component: './Dashboard/Analysis'},
+                    {path: 'monitor', component: './Dashboard/Monitor'},
+                    {path: 'workplace', component: './Dashboard/Workplace'},
+                ],
+            },
         ],
     }],
     plugins: [
         ['umi-plugin-react', {
+            Dva: true,
             antd: true,
         }],
     ],
