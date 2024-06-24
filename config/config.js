1fa00235c90077380f0a8786a4ebfc6ecb974313
espazo <espazo@outlook.com>
2024-06-24 18:32:01 +0800

    feature: add layout component

--- config/config.js
+++ config/config.js
@@ -2,10 +2,17 @@ export default {
     singular: true,
     routes: [{
         path: '/',
-        component: './HelloWorld'
+        component: '../layout/',
+        routes: [
+            {
+                path: '/helloworld',
+                component: './HelloWorld',
+            }
+        ],
     }],
     plugins: [
         ['umi-plugin-react', {
+            antd: true,
         }],
     ],
 };
