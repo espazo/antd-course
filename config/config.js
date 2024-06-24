1d485cedf33e71bbfb59a6844882db53c7399a6e
espazo <espazo@outlook.com>
2024-06-24 16:17:51 +0800

    config: create antd project with umi

---
+++ config/config.js
@@ -0,0 +1,11 @@
+export default {
+    singular: true,
+    routes: [{
+        path: '/',
+        component: './HelloWorld'
+    }],
+    plugins: [
+        ['umi-plugin-react', {
+        }],
+    ],
+};
