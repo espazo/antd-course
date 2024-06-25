62f86108895549582af0038a334ba9275d59b340
espazo <espazo@outlook.com>
2024-06-25 09:32:43 +0800

    feature: add card with dva

--- src/page/puzzlecards.jsx
+++ src/page/puzzlecards.jsx
@@ -5,67 +5,43 @@ import {connect} from 'dva';
 const namespace = 'puzzlecards';
 
 const mapStateToProps = (state) => {
-    const cardList = state[namespace];
+    const cardList = state[namespace].data;
     return {
         cardList,
     };
 }
 
-@connect(mapStateToProps)
-export default class PuzzlecardsPage extends Component {
-    constructor(props) {
-        super(props);
-        this.counter = 100;
-        this.state = {
-            cardList: [
-                {
-                    id: 1,
-                    setup: 'Did you hear about the two silk worms in a race?',
-                    punchline: 'It ended in a tie',
-                },
-                {
-                    id: 2,
-                    setup: 'What happens to a frog\'s car when it breaks down?',
-                    punchline: 'It gets toad away',
-                },
-            ],
-        };
-    }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onClickAdd: (newCard) => {
+            const action = {
+                type: `${namespace}/addNewCard`, payload: newCard,
+            };
+            dispatch(action);
+        }
+    };
+}
 
-    addNewCard = () => {
-        this.setState(prevState => {
-            const prevCardList = prevState.cardList;
-            this.counter += 1;
-            const card = {
-                id: this.counter,
-                setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-                punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            };
-            return {
-                cardList: prevCardList.concat(card),
-            };
-        });
-    }
+@connect(mapStateToProps, mapDispatchToProps)
+export default class PuzzlecardsPage extends Component {
 
     render() {
-        return (
-            <div>
-                {
-                    this.state.cardList.map(card => {
-                        return (
-                            <Card key={card.id}>
-                                <div>Q: {card.setup}</div>
-                                <div>
-                                    <strong>A: {card.punchline}</strong>
-                                </div>
-                            </Card>
-                        );
-                    })
-                }
+        return (<div>
+                {this.props.cardList.map(card => {
+                    return (<Card key={card.id}>
+                            <div>Q: {card.setup}</div>
+                            <div>
+                                <strong>A: {card.punchline}</strong>
+                            </div>
+                        </Card>);
+                })}
                 <div>
-                    <Button onClick={this.addNewCard}>add cards</Button>
+                    <Button onClick={() => this.props.onClickAdd({
+                        setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', punchline: 'here we use dva',
+                    })}>
+                        add cards
+                    </Button>
                 </div>
-            </div>
-        );
+            </div>);
     }
 }
