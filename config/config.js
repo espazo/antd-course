782a90641a48a4606570d116028e972ecbf188a7
espazo <espazo@outlook.com>
2024-06-25 14:58:57 +0800

    feature: add antd table

--- config/config.js
+++ config/config.js
@@ -1,31 +1,34 @@
 export default {
     singular: true,
-    routes: [{
-        path: '/',
-        component: '../layout/',
-        routes: [
-            {
-                path: 'puzzlecards',
-                component: './puzzlecards',
-            },
-            {
-                path: '/helloworld',
-                component: './HelloWorld',
-            },
-            {
-                path: '/',
-                component: './HelloWorld',
-            },
-            {
-                path: '/dashboard',
-                routes: [
-                    {path: 'analysis', component: './Dashboard/Analysis'},
-                    {path: 'monitor', component: './Dashboard/Monitor'},
-                    {path: 'workplace', component: './Dashboard/Workplace'},
-                ],
-            },
-        ],
-    }],
+    routes: [
+        {path: 'list', component: './list'},
+        {
+            path: '/',
+            component: '../layout/',
+            routes: [
+                {
+                    path: 'puzzlecards',
+                    component: './puzzlecards',
+                },
+                {
+                    path: '/helloworld',
+                    component: './HelloWorld',
+                },
+                {
+                    path: '/',
+                    component: './HelloWorld',
+                },
+                {
+                    path: '/dashboard',
+                    routes: [
+                        {path: 'analysis', component: './Dashboard/Analysis'},
+                        {path: 'monitor', component: './Dashboard/Monitor'},
+                        {path: 'workplace', component: './Dashboard/Workplace'},
+                    ],
+                },
+            ],
+        },
+    ],
     proxy: {
         '/dev': {
             target: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com',
